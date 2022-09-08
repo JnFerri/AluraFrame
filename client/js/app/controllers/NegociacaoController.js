4e39af4f9ec514a8de359365b167a087dfc68d95
Jean Ferri <jeanferrimh@gmail.com>
2022-09-08 19:37:37 -0300

    08/09/2022

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,8 +9,9 @@ class NegociacaoController {
         this._inputValor = $('#valor')
         this._listaNegociacoes = new ListaNegociacoes()
         this._negociacoesView = new NegociacoesView($('#negociacoesView'))
-
+        this._mensagem = new Mensagem()
         this._negociacoesView.update(this._listaNegociacoes)
+        this._mensagemView = new MensagemView($('#mensagemView'))
         
     }
 
@@ -18,6 +19,8 @@ class NegociacaoController {
         event.preventDefault()
         this._listaNegociacoes.adiciona(this._criaNegociacao())
          this._limpaFormulario()
+         this._mensagem.texto = 'Negociacao adicionada com sucesso'
+        this._mensagemView.update(this._mensagem)
          this._negociacoesView.update(this._listaNegociacoes)
 
             }
