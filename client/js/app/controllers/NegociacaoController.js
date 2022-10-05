3d6d234e86b1e89c7c6d3c3329fb5b3b2cd01cd4
Jean Ferri <jeanferrimh@gmail.com>
2022-10-05 20:41:20 -0300

    Ajax

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -32,6 +32,22 @@ class NegociacaoController {
         
             }
 
+    importarNegociacoes(){
+       let service = new NegociacaoService()
+
+       service.obterNegociacoesDaSemana((erro, negociacoes) => {
+        
+        if(erro){
+            this._mensagem.texto = erro
+            return
+        }
+
+        negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
+        this._mensagem.texto = `Negociaçoes importadas com sucesso`
+       })
+
+    }
+
             apagar(){
                 this._listaNegociacoes.esvazia()
                 this._mensagem.texto = 'Negociação apagada com sucesso'
---
+++ client/js/app/services/NegociacaoService.js
@@ -0,0 +1,35 @@
+class NegociacaoService{
+    obterNegociacoesDaSemana(cb){
+        let xhr = new XMLHttpRequest()
+
+        xhr.open('GET','negociacoes/semana')
+
+        xhr.onreadystatechange = () => {
+            /* Posseiveis estados de uma requisição AJAX
+            0: requisicao ainda nao iniciada
+            1: coneccao com o servidor estabelecida
+            2:requisicao recebida
+            3:processando requisicao
+            4:requisicao concluida e a resposta esta pronta
+            */ 
+        if(xhr.readyState == 4){
+            //status 200 e que a resposta e correta, as vezes o servidor pode mandar um erro que a requisicao aceita como valida
+            if(xhr.status == 200){
+
+                cb(null, JSON.parse(xhr.responseText)
+                .map(objeto => new Negociacao(new Date(objeto.data),objeto.quantidade,objeto.valor))
+                )
+    
+        
+            } else {
+                console.log(xhr.responseText)
+                cb(`Não foi possivel obter as negociações`, null)
+               
+            }
+        }
+        
+        }
+
+        xhr.send()
+    }
+}
